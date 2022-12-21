77bb43146e820c9746427fbea57475b2906fd402
Littlaa <83633992+Littlaa@users.noreply.github.com>
2022-12-21 19:16:19 +0100

    User profile is showing on profile page

---
+++ js/listings/profile.mjs
@@ -0,0 +1,46 @@
+import { API_AUCTION_URL } from "../api/constants.mjs";
+import { authFetch } from "../api/authFetch.mjs";
+import { load } from "../storage/index.mjs";
+
+const user = load("profile");
+const userName = user.name;
+
+const action = "/profiles";
+const method = "GET";
+
+const url = `${API_AUCTION_URL}${action}/${userName}`;
+const container = document.querySelector(".profileData");
+
+export async function getProfile() {
+  try {
+    const response = await authFetch(url, { method });
+    const info = await response.json();
+
+    container.innerHTML = `<div class="d-flex justify-content-center pt-4">
+    <div class="card">
+      <img src="${info.avatar}" class="card-img-top" height="300" />
+      <div class="card-body">
+        <h5 class="card-title mb-2">${info.name}</h5>
+        <h6 class="card-title mb-4">${info.email}</h6>
+        <p class="text-muted mb-3">Wins: ${info.wins.length}</p>
+        <p class="text-muted mb-3">Listings: ${info._count.listings}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Credit - ${info.credits}$</li>
+      </ul>
+    </div>
+  </div>`;
+
+    addTitle(info);
+
+    console.log(info);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function addTitle(info) {
+  document.title = "BidNBuy | " + info.name;
+}
+
+getProfile();
